80934ab189f38df8d47d2977df9cf6d6204fce91
Thieny Moda <thienyam@hotmail.com>
2026-08-19 20:11:14 -0300

    Category

---
+++ src/main/java/com/projeto/course/repositories/CategoryRepository.java
@@ -0,0 +1,13 @@
+package com.projeto.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.projeto.course.entities.Category;
+
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+	
+	
+	
+	
+
+}
